8a3e55f54107f74d854309a4cf13571c50117901
Doria15 <doriaceelin@gmail.com>
2023-11-13 21:11:17 +0800

    Updated Task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailEl = document.querySelector('label [name = "email"]');
+const messageEl = document.querySelector('label [name = "message"]');
+
+form.addEventListener('input', throttle(handleInput, 500));
+function handleInput(event) {
+  event.preventDefault();
+
+  const email = emailEl.value;
+  const message = messageEl.value;
+  const formData = {
+    email,
+    message,
+  };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+form.addEventListener('submit', handleSubmit);
+function handleSubmit(e) {
+  e.preventDefault();
+
+  const email = emailEl.value;
+  const message = messageEl.value;
+  const formData = {
+    email,
+    message,
+  };
+
+  console.log(formData);
+
+  localStorage.removeItem('feedback-form-state');
+
+  form.reset();
+}
+
+const onPageLoad = function () {
+  const localStorageValue = JSON.parse(
+    localStorage.getItem('feedback-form-state')
+  );
+  if (localStorageValue) {
+    emailEl.value = localStorageValue.email;
+    messageEl.value = localStorageValue.message;
+  }
+};
+onPageLoad();
